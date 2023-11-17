9f3b3555e7adc321e4a5ad9d77495aab3eb1795e
dongwook98 <ehddnr8563@gmail.com>
2023-11-18 01:07:21 +0900

    feat: 공통 레이아웃 상단 메뉴 추가

---
+++ prepare/front/components/AppLayout.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+
+const AppLayout = ({ children }) => {
+  return (
+    <div>
+      <div>
+        <Link href='/'>
+          <a>트위터</a>
+        </Link>
+        <Link href='/profile'>
+          <a>프로필</a>
+        </Link>
+        <Link href='/signup'>
+          <a>회원가입</a>
+        </Link>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+AppLayout.propTypes = {
+  children: PropTypes.node.isRequired, // node: 리액트의 노드 리턴안에 들어갈수있는것들이 모두 노드!
+};
+export default AppLayout;

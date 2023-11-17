db2db85768e08cc8cf6d451b8993a31283f4216a
dongwook98 <ehddnr8563@gmail.com>
2023-11-18 07:12:28 +0900

    feat: 로그인 폼 컴포넌트 구현

---
+++ prepare/front/components/LoginForm.js
@@ -0,0 +1,48 @@
+import React, { useState, useCallback } from 'react';
+import { Form, Input, Button } from 'antd';
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const ButtonWrapper = styled.div`
+  margin-top: 10px;
+`;
+
+const LoginForm = () => {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChangeId = useCallback((e) => {
+    setId(e.target.value);
+  }, []);
+
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.value);
+  }, []); // 너무 반복됨.. 라이브러리 고려 or 커스텀 훅 고려
+
+  return (
+    <Form>
+      <div>
+        <label htmlFor='user-id'>아이디</label>
+        <br />
+        <Input name='user-id' value={id} onChange={onChangeId} required />
+      </div>
+      <div>
+        <label htmlFor='user-password'>아이디</label>
+        <br />
+        <Input name='user-password' value={password} onChange={onChangePassword} required />
+      </div>
+      <ButtonWrapper>
+        <Button type='primary' htmlType='submit' loading={false}>
+          로그인
+        </Button>
+        <Link href='/signup'>
+          <a>
+            <Button>회원가입</Button>
+          </a>
+        </Link>
+      </ButtonWrapper>
+    </Form>
+  );
+};
+
+export default LoginForm;

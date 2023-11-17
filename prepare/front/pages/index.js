6ed52b563461d1e6e491924689b773fa33ec794c
dongwook98 <ehddnr8563@gmail.com>
2023-11-18 01:06:36 +0900

    feat: 페이지 컴포넌트 추가

---
+++ prepare/front/pages/index.js
@@ -0,0 +1,13 @@
+// import React from 'react'; next는 없어도됨~
+import React from 'react';
+import AppLayout from '../components/AppLayout';
+
+const Home = () => {
+  return (
+    <AppLayout>
+      <div>Hello, Next!</div>
+    </AppLayout>
+  );
+};
+
+export default Home;

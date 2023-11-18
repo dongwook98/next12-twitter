bf6e0f0586fc73d801dfe3c529fdfebd5d5633f4
dongwook98 <ehddnr8563@gmail.com>
2023-11-18 10:02:44 +0900

    feat:  로그인 시 나타나는 유저 프로필 컴포넌트 구현

---
+++ prepare/front/components/UserProfile.js
@@ -0,0 +1,34 @@
+import React, { useCallback } from 'react';
+import { Card, Avatar, Button } from 'antd';
+import styled from 'styled-components';
+
+const ButtonWrapper = styled(Button)`
+  margin-left: 47px;
+`;
+
+const UserProfile = ({ setIsLoggedIn }) => {
+  const onLogout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+  return (
+    <Card
+      actions={[
+        <div key='twit'>
+          게시글 <br />0
+        </div>,
+        <div key='following'>
+          팔로잉
+          <br />0
+        </div>,
+        <div key='follower'>
+          팔로워
+          <br />0
+        </div>,
+      ]}>
+      <Card.Meta avatar={<Avatar>동욱</Avatar>} title='dongwook' />
+      <ButtonWrapper onClick={onLogout}>로그아웃</ButtonWrapper>
+    </Card>
+  );
+};
+
+export default UserProfile;

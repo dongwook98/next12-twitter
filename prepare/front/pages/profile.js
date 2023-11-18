f0c8a58bc6296118759942d99d8cb803a5e403ef
dongwook98 <ehddnr8563@gmail.com>
2023-11-18 10:04:26 +0900

    chore: 프로필 페이지에 닉네임 수정 컴포넌트와 팔로잉, 팔로워 리스트 컴포넌트 추가

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
+import FollowList from '../components/FollowList';
+import NicknameEditForm from '../components/NicknameEditForm';
 
 const Profile = () => {
+  const followerList = [{ nickname: '김예리' }, { nickname: '이태인' }, { nickname: '전우진' }];
+  const followingList = [{ nickname: '김예리' }, { nickname: '이태인' }, { nickname: '전우진' }];
   return (
     <>
       <Head>
@@ -10,7 +14,9 @@ const Profile = () => {
         <title>내 프로필 | 트위터</title>
       </Head>
       <AppLayout>
-        <div>내 프로필</div>
+        <NicknameEditForm />
+        <FollowList header='팔로잉 목록' data={followingList} />
+        <FollowList header='팔로워 목록' data={followerList} />
       </AppLayout>
     </>
   );

48872481afd3e03382a79f80c98c7ceaf18f92fb
dongwook98 <ehddnr8563@gmail.com>
2023-11-20 06:51:21 +0900

    chore: user 속성명 변경

--- prepare/front/reducers/user.js
+++ prepare/front/reducers/user.js
@@ -1,6 +1,6 @@
 export const initailState = {
   isLoggedIn: false,
-  user: null,
+  me: null,
   signUpData: {},
   loginData: {},
 };
@@ -25,13 +25,13 @@ const reducer = (state = initailState, action) => {
       return {
         ...state,
         isLoggedIn: true,
-        user: action.data,
+        me: action.data,
       };
     case 'LOG_OUT':
       return {
         ...state,
         isLoggedIn: false,
-        user: null,
+        me: null,
       };
     default:
       return state;

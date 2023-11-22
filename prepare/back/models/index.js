1d8b9d432047b7ae1c63fd5da2b2bf45dd010cc6
dongwook98 <ehddnr8563@gmail.com>
2023-11-23 01:12:17 +0900

    chore: 시퀄라이즈 사용해서 노드와 MY SQL 연결

---
+++ prepare/back/models/index.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development'; // 기본으로 development
+const config = require('../config/config')[env];
+const db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config); // 시퀄라이즈가 노드랑 mysql을 연결해주는걸 도와줌 // 시퀄라이즈는 내부적으로 mysql2를 사용해서 mysql2 드라이버에 설정 정보들을 보내줌
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;

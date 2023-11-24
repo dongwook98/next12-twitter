bb854442ca3a2242ccb9d7df5f0211da4330cc58
dongwook98 <ehddnr8563@gmail.com>
2023-11-24 22:43:13 +0900

    feat: 패스포트 로그인 시 세션 설정

--- prepare/back/passport/index.js
+++ prepare/back/passport/index.js
@@ -1,10 +1,22 @@
 const passport = require('passport');
 const local = require('./local');
+const { User } = require('../models');
 
 module.exports = () => {
-  passport.serializeUser(() => {});
+  passport.serializeUser((user, done) => {
+    done(null, user.id); // 세션에 user.id만 저장
+  });
 
-  passport.deserializeUser(() => {});
+  // 세션에 저장된 user.id로 DB에서 찾기
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findOne({ where: { id } });
+      done(null, user); // req.user
+    } catch (error) {
+      console.error(error);
+      done(error);
+    }
+  });
 
   local();
 };

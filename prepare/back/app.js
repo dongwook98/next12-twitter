fe57e56c9ec8420191d0058cd36817eb7f60a84f
dongwook98 <ehddnr8563@gmail.com>
2023-11-24 22:43:54 +0900

    chore: .env 적용 및 쿠키/세션 설정

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 // 분리한 라우터들 불러오기
+const passport = require('passport');
+const dotenv = require('dotenv');
+
 const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
 // models/index.js에 있는 db 불러오기
@@ -8,6 +13,7 @@ const db = require('./models');
 // 패스포트 설정 불러오기
 const passportConfig = require('./passport');
 
+dotenv.config();
 const app = express();
 
 // 서버 실행할 때 DB,시퀄라이즈 연결
@@ -30,6 +36,16 @@ app.use(
 // 라우터들의 위에 위치해야한다. 순서 중요
 app.use(express.json()); // 프론트에서 json형식으로 데이터를 보냈을때 json형식의 데이터를 req.body안에 넣을 수 있게 처리
 app.use(express.urlencoded({ extended: true })); // 폼 서브밋을 했을때 url encoded 방식으로 데이터가 넘어오는데 그 데이터를 req.body 안에 넣을 수 있게 처리
+
+// 세션/쿠키 설정
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+  saveUninitialized: false,
+  resave: false,
+  secret: process.env.COOKIE_SECRET,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.send('hello express');

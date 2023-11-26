97ffdb6aaaf05a50ce08e88224798baf6b8c1cf2
dongwook98 <ehddnr8563@gmail.com>
2023-11-26 23:23:36 +0900

    chore: 주석 추가

--- prepare/front/pages/index.js
+++ prepare/front/pages/index.js
@@ -1,4 +1,4 @@
-// import React from 'react'; next는 없어도됨~
+// import React from 'react'; next에서는 생략 가능
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import AppLayout from '../components/AppLayout';
@@ -11,6 +11,7 @@ const Home = () => {
   const { me } = useSelector((state) => state.user);
   const { mainPosts, hasMorePosts, loadPostsLoading } = useSelector((state) => state.post);
 
+  // Home 컴포넌트 마운트시 LOAD_POSTS_REQUEST 액션 실행 -> Saga에 등록해둔 loadPosts 실행(서버와 통신) -> 성공시 LOAD_POSTS_SUCCESS 액션 실행 + 서버에서 받아온 데이터 reducer에 전달 -> reducer에 적어준대로 상태 업데이트
   useEffect(() => {
     dispatch({
       type: LOAD_POSTS_REQUEST,
@@ -28,6 +29,8 @@ const Home = () => {
       }
     }
     window.addEventListener('scroll', onScroll);
+
+    // 컴포넌트 언마운트시 스크롤 이벤트 제거
     return () => {
       window.removeEventListener('scroll', onScroll);
     };
@@ -35,7 +38,9 @@ const Home = () => {
 
   return (
     <AppLayout>
+      {/* 로그인했을때는 PostForm 컴포넌트 활성화 */}
       {me && <PostForm />}
+      {/* 게시글들 map()으로 매핑 */}
       {mainPosts.map((post) => (
         <PostCard key={post.id} post={post} />
       ))}

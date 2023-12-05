350b3dc0097bee2ddebf52501e6bd03d2942fb46
dongwook98 <ehddnr8563@gmail.com>
2023-12-05 14:32:24 +0900

    feat: 게시글 해시태그 작성 db 등록

--- prepare/back/routes/post.js
+++ prepare/back/routes/post.js
@@ -5,7 +5,7 @@ const path = require('path');
 // 파일 시스템을 조작할 수 있음
 const fs = require('fs');
 
-const { Post, Comment, Image, User } = require('../models');
+const { Post, Comment, Image, User, Hashtag } = require('../models');
 const { isLoggedIn } = require('./middlewares');
 
 const router = express.Router();
@@ -42,14 +42,26 @@ const upload = multer({
  */
 router.post('/', isLoggedIn, upload.none(), async (req, res, next) => {
   try {
+    const hashtags = req.body.content.match(/#[^\s#]+/g);
     const post = await Post.create({
       content: req.body.content,
-      // 라우터에 접근하면 deserializeUser가 실행되서 사용자 정보를 복구해서 req.user에 넣어준다.
+      // 라우터에 접근하면 deserializeUser가 실행되서 사용자 정보를 복구해서 req.user에 넣어주어서 req.user 사용가능
       UserId: req.user.id,
     });
+    if (hashtags) {
+      const result = await Promise.all(
+        hashtags.map((tag) =>
+          Hashtag.findOrCreate({
+            where: { name: tag.slice(1).toLowerCase() },
+          })
+        )
+      ); // [노드, true], [리액트, true]
+      await post.addHashtags(result.map((v) => v[0]));
+    }
     if (req.body.image) {
       if (Array.isArray(req.body.image)) {
         // 이미지를 여러 개 올리면 image: [제로초.png, 부기초.png]
+        // Promise.all을 사용해 이미지 주소를 한방에 DB에 저장
         const images = await Promise.all(req.body.image.map((image) => Image.create({ src: image })));
         await post.addImages(images);
       } else {
